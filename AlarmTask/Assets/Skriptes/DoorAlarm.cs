fa90258139deea603b01cc6332454c325463cdc7
TapAlexxx <lordpictures123@gmail.com>
2020-08-18 18:27:10 +0300

    Alarm

---
+++ AlarmTask/Assets/Skriptes/DoorAlarm.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource),typeof(Rigidbody2D))]
+public class DoorAlarm : MonoBehaviour
+{
+    [SerializeField] private float _timeToFullAlarmVolume;
+    [SerializeField] private ContactFilter2D _filter;
+
+    private AudioSource _audioSource;
+    private bool _isThiefInHous;
+    private float _currentAlarmDuration;
+    private Rigidbody2D _rigidbody2D;
+
+    private readonly RaycastHit2D[] _results = new RaycastHit2D[1];
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_isThiefInHous)
+        {
+            _currentAlarmDuration += Time.deltaTime;
+            float normalizedAlarmVolume = _currentAlarmDuration / _timeToFullAlarmVolume;
+            _audioSource.volume = normalizedAlarmVolume;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var collisionCount = _rigidbody2D.Cast(transform.right, _filter, _results);
+
+        if (collisionCount > 0)
+        {
+            Alarm();
+        }
+        else
+        {
+            _isThiefInHous = false;
+            _audioSource.Stop();
+        }
+    }
+
+    private void Alarm()
+    {
+        _isThiefInHous = true;
+        if (!_audioSource.isPlaying)
+        {
+            _currentAlarmDuration = 0;
+            _audioSource.Play();
+        }
+    }
+}
---
+++ AlarmTask/Assets/Skriptes/PlayerMovement.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D),typeof(SpriteRenderer))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speedX;
+
+    public float MinGroundNormalY = 0.65f;
+    public float GravityModifier = 1f;
+    public Vector2 Velocity;
+    public LayerMask LayerMask;
+
+    private Vector2 _targetVelocity;
+    private bool _grounded;
+    private Vector2 _groundNormal;
+    private Rigidbody2D _rigidbody2D;
+    private ContactFilter2D _contactFilter;
+    private RaycastHit2D[] _hitBuffer = new RaycastHit2D[16];
+    private List<RaycastHit2D> _hitBufferList = new List<RaycastHit2D>(16);
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private const float _minMoveDistance = 0.001f;
+    private const float _shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _contactFilter.useTriggers = false;
+        _contactFilter.SetLayerMask(LayerMask);
+        _contactFilter.useLayerMask = true;
+    }
+
+    private void Update()
+    {
+        _targetVelocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKey(KeyCode.Space) && _grounded)
+            Velocity.y = 7;
+    }
+
+    private void FixedUpdate()
+    {
+        Velocity += GravityModifier * Physics2D.gravity * Time.deltaTime;
+        Velocity.x = _targetVelocity.x * _speedX;
+
+        _grounded = false;
+
+        Vector2 deltaPosition = Velocity * Time.deltaTime;
+        Vector2 moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement(move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+
+        AnimatePlayerMovement();
+    }
+
+    private void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if (distance > _minMoveDistance)
+        {
+            int count = _rigidbody2D.Cast(move, _contactFilter, _hitBuffer, distance + _shellRadius);
+
+            _hitBufferList.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                _hitBufferList.Add(_hitBuffer[i]);
+            }
+
+            for (int i = 0; i < _hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = _hitBufferList[i].normal;
+                if (currentNormal.y > MinGroundNormalY)
+                {
+                    _grounded = true;
+                    if (yMovement)
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(Velocity, currentNormal);
+                if (projection < 0)
+                {
+                    Velocity = Velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = _hitBufferList[i].distance - _shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        _rigidbody2D.position = _rigidbody2D.position + move.normalized * distance;
+    }
+
+    private void AnimatePlayerMovement()
+    {
+        if (Velocity.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        if (Velocity.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        _animator.SetFloat("SpeedX", Mathf.Abs(Velocity.x));
+        _animator.SetFloat("SpeedY", Velocity.y);
+    }
+}
